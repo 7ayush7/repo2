b61e3160325af6ac38405a599a34f65a1f1b30d3
7ayush7 <7ayush7@gmail.com>
2020-04-24 22:30:09 +0530

    Commit 1

---
+++ mavenDemo/src/main/java/com/amdocs/ayush/mavenDemo/App.java
@@ -0,0 +1,13 @@
+package com.amdocs.ayush.mavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
