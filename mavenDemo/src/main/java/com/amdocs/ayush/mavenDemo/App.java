c9a298f79b06291c43ecc3acf824c80fc00837b0
7ayush7 <7ayush7@gmail.com>
2020-04-25 12:59:41 +0530

    failure commit

--- mavenDemo/src/main/java/com/amdocs/ayush/mavenDemo/App.java
+++ mavenDemo/src/main/java/com/amdocs/ayush/mavenDemo/App.java
@@ -6,7 +6,7 @@ package com.amdocs.ayush.mavenDemo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(asa)
     {
         System.out.println( "Hello World!" );
            
