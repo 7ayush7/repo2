8c994ff159a6edc3a94eb9de2f8893bf35a3ebfc
7ayush7 <7ayush7@gmail.com>
2020-04-25 15:26:30 +0530

    Failure Resolved

--- mavenDemo/src/main/java/com/amdocs/ayush/mavenDemo/App.java
+++ mavenDemo/src/main/java/com/amdocs/ayush/mavenDemo/App.java
@@ -1,13 +1,12 @@
 package com.amdocs.ayush.mavenDemo;
 
-import test;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main(asa)
+    public static void main(String[] args)
     {
         System.out.println( "Hello World!" );
            
