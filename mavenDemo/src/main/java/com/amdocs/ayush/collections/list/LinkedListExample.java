0f93420b4a729368c401802f936a54c5d65c6103
7ayush7 <7ayush7@gmail.com>
2020-04-26 21:54:25 +0530

    LinkedList example

---
+++ mavenDemo/src/main/java/com/amdocs/ayush/collections/list/LinkedListExample.java
@@ -0,0 +1,43 @@
+package com.amdocs.ayush.collections.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+
+	public static void main(String[] args){
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		Integer[] i = new Integer[]{1,2};
+		List<Integer> list2 = new ArrayList<Integer>();
+		list2=Arrays.asList(i);
+		list.addAll(list2);
+		list.addFirst(Integer.valueOf(0));
+		list.addFirst(Integer.valueOf(-1));
+		System.out.println(list);
+		Integer[] toArray = new Integer[list.size()];
+		toArray = list.toArray(toArray);
+		System.out.println(toArray);
+		LinkedList<Integer> list3 = (LinkedList<Integer>) list.clone();
+		System.out.println(list3);
+		System.out.println(list3.contains(1));
+		System.out.println(list3.equals(list));
+		System.out.println(list.containsAll(list3));
+		list.push(1);
+		System.out.println(list);
+		System.out.println(list.pop());
+		System.out.println(list);
+		System.out.println(list.peek());
+		System.out.println(list);
+		Iterator<Integer> iter = list.descendingIterator();
+		//Iterates the list in reverse order
+		Collections.reverse(list3);
+		System.out.println(list3);
+		while(iter.hasNext()){
+			System.out.println(iter.next());
+		}
+	}
+}
