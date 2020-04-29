835fdf842df60cf0f06e0b0f6eceb49f4ce7555c
7ayush7 <7ayush7@gmail.com>
2020-04-29 21:45:25 +0530

    Immutable Example

---
+++ mavenDemo/src/main/java/com/amdocs/ayush/immutable/Address.java
@@ -0,0 +1,15 @@
+package com.amdocs.ayush.immutable;
+
+public class Address implements Cloneable{
+
+	String streetName;
+	
+	Address(){}
+	Address(String streetName){
+		this.streetName=streetName;
+	}
+	protected Object clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+	
+}
---
+++ mavenDemo/src/main/java/com/amdocs/ayush/immutable/Employee.java
@@ -0,0 +1,28 @@
+package com.amdocs.ayush.immutable;
+
+public final class Employee {
+
+	private final int id;
+	private final String name;
+	private final Address add;
+	
+	public Employee(int id, String name, Address add) throws CloneNotSupportedException {
+		this.id=id;
+		this.name=name;
+		this.add=(Address) add.clone();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Address getAdd() throws CloneNotSupportedException {
+		return (Address) this.add.clone();
+	}
+	
+	
+}
