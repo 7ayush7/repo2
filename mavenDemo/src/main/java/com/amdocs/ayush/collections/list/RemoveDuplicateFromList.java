1bff61754c57eab2abc5a8ffa0cc93511bafd3a4
7ayush7 <7ayush7@gmail.com>
2020-05-07 23:27:48 +0530

    RemovingDuplicates from List Example

---
+++ mavenDemo/src/main/java/com/amdocs/ayush/collections/list/RemoveDuplicateFromList.java
@@ -0,0 +1,40 @@
+package com.amdocs.ayush.collections.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicateFromList {
+
+	public static void main(String[] args){
+		List<Integer> list = Arrays.asList(1,2,3,1,1);
+		System.out.println(list);
+		//1st way
+		List<Integer> s = list.stream().distinct().collect(Collectors.toList());
+		System.out.println(s);
+		
+		//2nd way
+		Set<Integer> set = new HashSet<>(list);
+		System.out.println(set);
+		
+		//3rd way
+		List<Integer> dup = new ArrayList<>();//to list out duplicates
+		List<Integer> dup2 = new ArrayList<>();//to store duplicates
+
+		for(Integer i : list){
+			if(!dup.contains(i)){
+				dup.add(i);
+			}
+			else{
+				if(!dup2.contains(i)){
+					dup2.add(i);
+				}
+			}
+		}
+		System.out.println(dup);
+		System.out.println(dup2);
+	}
+}
