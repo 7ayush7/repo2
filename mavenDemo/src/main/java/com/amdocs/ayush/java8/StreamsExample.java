c390970999f9556208da84360d13d1ef9767bdc7
7ayush7 <7ayush7@gmail.com>
2020-05-10 12:05:09 +0530

    Stream Update

--- mavenDemo/src/main/java/com/amdocs/ayush/java8/StreamsExample.java
+++ mavenDemo/src/main/java/com/amdocs/ayush/java8/StreamsExample.java
@@ -3,6 +3,9 @@ package com.amdocs.ayush.java8;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 class Student{
@@ -22,7 +25,16 @@ public class StreamsExample {
 
 	public static void main(String[] args){
 		
-		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		List<Integer> list = Arrays.asList(1,2,3,4,5,1,1);
+		System.out.println(list);
+		System.out.println(list.stream().distinct().collect(Collectors.toList()));
+		System.out.println(list.stream().collect(Collectors.toSet()));
+		
+		List<Integer> dummyList = Arrays.asList(1000,1000,2222,121212);
+		OptionalDouble avg = dummyList.stream().mapToInt(n->n*n).filter(t->t>1000).average();
+		if(avg.isPresent()){
+			System.out.println(avg);
+		}
 		List<Integer> list2 = list.stream().filter((t)->(t>4)).collect(Collectors.toList());
 		System.out.println(list2);
 		
@@ -48,5 +60,6 @@ public class StreamsExample {
 		System.out.println();
 		List<Integer> list6 = Arrays.asList(1,1,1,2,3,4,5,6,6,7,8);
 		list6.stream().distinct().filter(s->s>3).map(a->a).collect(Collectors.toList()).forEach(a->System.out.println(a));
+	
 	}
 }
