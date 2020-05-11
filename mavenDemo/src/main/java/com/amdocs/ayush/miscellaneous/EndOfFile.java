c4eba6e37359ae2105e1d66df89615d7ad1dc3b3
7ayush7 <7ayush7@gmail.com>
2020-05-11 20:35:27 +0530

    EndOfFileUpdate

--- mavenDemo/src/main/java/com/amdocs/ayush/miscellaneous/EndOfFile.java
+++ mavenDemo/src/main/java/com/amdocs/ayush/miscellaneous/EndOfFile.java
@@ -11,10 +11,10 @@ public class EndOfFile {
 		StringBuilder build = new StringBuilder();
 		int lineNumber = 0;
 		while(sc.hasNext()){
+			lineNumber++;
 			build.append(lineNumber+" ");
 			build.append(sc.nextLine());
 			build.append(System.lineSeparator());
-			lineNumber++;
 		}
 		System.out.println(build.toString());
 	}
